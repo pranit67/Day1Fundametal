51a7c6a0649a3229ba26d5b0dcbd1e87e9601a8d
pranit67 <pranit67@gmail.com>
2022-11-14 17:57:33 +0530

    PrimitiveDataType

---
+++ src/com/bridgelabz/fundamental/PrimetiveDataType.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.fundamental;
+
+public  class PrimetiveDataType {
+	static Long abc;
+	static boolean tf;
+	static byte gh;
+	static float hgg;
+	static int gdgdg;
+	static String hshddhString;
+	public static void main(String[] args) {
+		System.out.println("long default value="+ abc);
+		System.out.println("boolean default value="+ tf);
+		System.out.println("byte default value="+ gh);
+		System.out.println("float default value="+ hgg);
+		System.out.println("int default value="+ gdgdg);
+		System.out.println("String default value="+ hshddhString);
+	}
+}
