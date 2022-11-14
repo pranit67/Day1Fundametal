50213e95dcd1812846a1f61b004f187005b59fa6
pranit67 <pranit67@gmail.com>
2022-11-14 18:26:07 +0530

    UC4-NameUsingCammandLine

---
+++ src/com/bridgelabz/fundamental/NameUsingCammandLine.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.fundamental;
+
+public class NameUsingCammandLine {
+	public static void main(String[] args) {
+		System.out.println("Command Line Program..!!");
+		for (String str : args) {
+			System.out.println(str);
+		}
+	}
+
+}
